ff11ee4527a3c3c3c33380727f92b29275f02bf5
hariganet <hariganet.sandbox@gmail.com>
2013-11-25 03:46:03 +0900

    test error ocured for foever.js

--- nodeByGihyo/http/app.js
+++ nodeByGihyo/http/app.js
@@ -1,7 +1,9 @@
+var n = 0;
 var http = require('http');
 http.createServer(function(req, res){
+  if(++n > 4){ a }
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end("result:" + n);
 }).listen(3000, "127.0.0.1");
-console.log('Server running at http://localhost/');
+console.log('Server running at http://localhost:3000/');
 
