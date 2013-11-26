76a40b05c4700518e3f25f1155c558f44c85f9d0
hariganet <hariganet.sandbox@gmail.com>
2013-11-27 00:43:48 +0900

    create form

--- nodeByGihyo/express/firstapp/app.js
+++ nodeByGihyo/express/firstapp/app.js
@@ -29,6 +29,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/form', routes.form);
+app.post('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- nodeByGihyo/express/firstapp/routes/main.js
+++ nodeByGihyo/express/firstapp/routes/main.js
@@ -12,3 +12,12 @@ var items = [
 exports.index = function(req, res){
   res.render('index', { title: 'Entry List', items: items });
 };
+
+exports.form = function(req, res){
+  res.render('form', { title: 'New Entry' })
+};
+
+exports.create = function(req, res){
+  console.log(req.body.text);
+  res.redirect('/');
+};
