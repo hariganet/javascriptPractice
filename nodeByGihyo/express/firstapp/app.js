688138b3b72d2bda1ffb9f19998f2fcb713a4ff1
hariganet <hariganet.sandbox@gmail.com>
2013-12-01 21:19:13 +0900

    add mongoose

--- nodeByGihyo/express/firstapp/app.js
+++ nodeByGihyo/express/firstapp/app.js
@@ -17,7 +17,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
