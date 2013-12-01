d9e9731015685a72c178940f409fc4b5bce59106
hariganet <hariganet.sandbox@gmail.com>
2013-12-01 20:58:58 +0900

    setting grunt & dotinstal lesson making html site

--- htmlLesson/Gruntfile.js
+++ htmlLesson/Gruntfile.js
@@ -1,38 +1,47 @@
-// 'use strict';
+//'use strict;
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    //grunt-contrib-connectの設定(webサーバの設定)
+  
+    // grunt-contrib-connectの設定(Webサーバの設定)
     connect: {
-      site: { // オプション未設定のため、からオブジェクト
-        options: {
-          hostname: '*',
-          port: 8000
+      site: { // オプション未設定の為、空オブジェクト
+      }
+    },
+
+    jade:{
+      compile: {
+        files:{
+          'index.html': 'index.jade' 
         }
-      } 
+      }
     },
 
     // grunt-contrib-watchの設定(ウォッチ対象の設定)
     watch: {
       static_files: {
-        files: ['**/*.html','**/*.css'] //ウォッチ対象として、ディレクトリは以下の*.htmlを指定
+        files: ['**/*.html', '**/*.css']  // ウォッチ対象として、ディレクトリ配下の*.htmlを指定
       },
-      js_files: {
-        files: '**/*.js'
+      jade_files:{
+        files: '**/*.jade',
+        tasks: ['jade']
       },
       options: {
-        livereload: true  // 変更があればリロードするよ  
-      }  
+        livereload: true // 変更があればリロードするよ
+      }
     }
   });
-
+  
   // Load tasks(grunt実行時に読み込むプラグイン)
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+
 
   // Default tasks(grunt実行時に実行するタスク)
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('default', ['connect', 'jade', 'watch']);
+
 };
