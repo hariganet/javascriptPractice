81cd799faea9e5a052466f7a34c38ce5cc780b98
hariganet <hariganet.sandbox@gmail.com>
2013-12-03 21:40:29 +0900

    change ip to 0.0.0.0

--- nodeByGihyo/http/app.js
+++ nodeByGihyo/http/app.js
@@ -4,6 +4,6 @@ http.createServer(function(req, res){
   if(++n > 4){ a }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("result:" + n);
-}).listen(3000, "127.0.0.1");
+}).listen(3000, "0.0.0.0");
 console.log('Server running at http://localhost:3000/');
 
